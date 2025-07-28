3184a8b4e6f312958f515615feec7c8d804a06b5
Ryan <ryangadinga90@gmail.com>
2025-07-28 19:24:07 +0700

    add package-lock.json; update html & js (change name & age)

--- src/js/components/my-element.js
+++ src/js/components/my-element.js
@@ -1,5 +1,29 @@
-import { LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 
-class MyElement extends LitElement {}
+class MyElement extends LitElement {
+  static properties = {
+    name: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+  };
+
+  constructor() {
+    super();
+
+    this.name = 'John Doe';
+    this.age = 30;
+  }
+
+  render() {
+    console.log('render');
+    return html`
+      <p>Selamat Datang!</p>
+      <p>${this.name}, ${this.age}</p>
+    `;
+  }
+}
 
 customElements.define('my-element', MyElement);
